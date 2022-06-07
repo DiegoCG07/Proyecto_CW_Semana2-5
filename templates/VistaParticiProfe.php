708a7d36d1c6d8621d394985a5988254ff3a3893
Ricardo723 <ricardo.675@comunidad.unam.mx>
2022-06-06 20:40:09 -0500

    feat(templates): Añadiendo vistas del profesor

---
+++ templates/VistaParticiProfe.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Participantes</title>
+    <link rel="stylesheet" href="../statics/styles/VistaParticiProfe.css">
+    <link rel="stylesheet" href="../libs/bootstrap-5.2.0-beta1-dist/css/bootstrap.css">
+</head>
+<body>
+    <nav class="navbar bg-light fixed-top">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" id="titulo">Coyo 6</a>
+
+            <button id="boton" class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Opciones</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Perfil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="https://www.prepa6.unam.mx/ENP6/_P6/">Conoce nuestro plantel</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="https://www.prepa6.unam.mx/ENP6/_P6/">Prepa 6</a>
+                        </li>
+                    </ul>
+                
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div id="titulo2">
+        <br>
+        <span>Participantes</span><br>
+        <span>Materia Grupo</span><br>
+        <span>Nombre Profesor/a</span>
+    </div>
+
+    <div id="borde">
+        <div id="alumnos">
+            <span>Alumnos</span>
+        </div>
+
+        <div class="descripcionAlumnos">
+            <span>Nombre del alumno: </span><br>
+            <span>Correo: </span><br>
+            <span>Número de cuenta: </span>
+        </div>
+        <div class="descripcionAlumnos">
+            <span>Nombre del alumno: </span><br>
+            <span>Correo: </span><br>
+            <span>Número de cuenta: </span>
+        </div>
+        <div class="descripcionAlumnos">
+            <span>Nombre del alumno: </span><br>
+            <span>Correo: </span><br>
+            <span>Número de cuenta: </span>
+        </div>
+        <div class="descripcionAlumnos">
+            <span>Nombre del alumno: </span><br>
+            <span>Correo: </span><br>
+            <span>Número de cuenta: </span>
+        </div>
+        <div class="descripcionAlumnos">
+            <span>Nombre del alumno: </span><br>
+            <span>Correo: </span><br>
+            <span>Número de cuenta: </span>
+        </div>
+    </div>
+
+    <div class="opciones">
+        <div id="calendario">
+            <span>Calendario</span>
+        </div>
+        <div id="participantes">
+            <span>Participantes</span>
+        </div>
+        <div id="contacto">
+            <span>Contacta a tu alumno</span>
+        </div>
+    </div>
+
+    
+
+
+    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js "></script>
+</body>
+</html>
